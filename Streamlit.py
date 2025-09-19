4d961d46d10567c61301d00e9cf74023f510cf02
Tien Le <QNFT43@motorolasolutions.com>
2025-09-19 08:17:10 -0500

    Update Streamlit.py with new code

--- Streamlit.py
+++ Streamlit.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import os
 
-st.write("Hello")
+PASSWORD = os.getenv("APP_PASSWORD", "default123")
+
+pw = st.text_input("Enter password", type="password")
+if pw != PASSWORD:
+    st.stop()
+
+st.write("Welcome! You are logged in.")
