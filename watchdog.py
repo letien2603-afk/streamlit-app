39d4c88e2c41bc1c5824cdbc24c1101e4ed2311a
letien2603-afk <letien2603@gmail.com>
2025-11-05 15:27:10 -0600

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 while True:
-    process = subprocess.Popen(["streamlit", "run", "streamlit.py"])
+    process = subprocess.Popen(["streamlit", "run", "Streamlit.py"])
     process.wait()
     print("App crashed. Restarting in 3 seconds...")
     time.sleep(3)
