ad2151de306c85d104de204ae8e237b8b0ad8fbe
letien2603-afk <letien2603@gmail.com>
2025-11-05 15:49:24 -0600

    Delete watchdog.py

--- watchdog.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import time
-
-while True:
-    process = subprocess.Popen(["streamlit", "run", "Streamlit.py"])
-    process.wait()
-    print("App crashed. Restarting in 3 seconds...")
-    time.sleep(3)
